0b8b756528a983648da735937b4238b2ceb2c847
Han Young <hanyoung@protonmail.com>
2022-06-22 19:34:05 +0800

    edit env

--- go/pkg/utility/configuration.go
+++ go/pkg/utility/configuration.go
@@ -4,18 +4,11 @@ import (
 	"os"
 )
 
-var UrlRoot = "http://127.0.0.1"
 var DatabasePath = "test.sqlite3"
 var DebugEnvironment = true
 
 func InitConfig() {
-	urlRoot := os.Getenv("LOGI_TRACKER_URLROOT")
-	databasePath := os.Getenv("LOGI_TRACKER_URLROOT")
-
-	if len(urlRoot) > 0 {
-		UrlRoot = urlRoot
-		DebugEnvironment = false
-	}
+	databasePath := os.Getenv("LOGI_TRACKER_DATABASE_PATH")
 	if len(databasePath) > 0 {
 		DatabasePath = databasePath
 		DebugEnvironment = false
